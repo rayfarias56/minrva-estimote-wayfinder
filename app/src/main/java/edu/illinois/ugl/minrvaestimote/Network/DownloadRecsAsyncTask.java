cf017450eef9caee6db5375767369e1788a0f8bb
Yier Huang <yhuang98@illinois.edu>
2016-02-12 21:29:49 -0600

    Added layout elements, default icons; Added click events for popular items;

--- app/src/main/java/edu/illinois/ugl/minrvaestimote/Network/DownloadRecsAsyncTask.java
+++ app/src/main/java/edu/illinois/ugl/minrvaestimote/Network/DownloadRecsAsyncTask.java
@@ -1,7 +1,11 @@
 package edu.illinois.ugl.minrvaestimote.Network;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -17,7 +21,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import edu.illinois.ugl.minrvaestimote.ExtendedSimpleAdapter;
+import edu.illinois.ugl.minrvaestimote.MainActivity;
 import edu.illinois.ugl.minrvaestimote.R;
+import edu.illinois.ugl.minrvaestimote.WebActivity;
 
 /**
  * Created by yierh on 12/1/15.
@@ -33,6 +40,8 @@ public class DownloadRecsAsyncTask extends AsyncTask<String, Void, JSONArray> {
     private List<Map<String, Object>> recEbookList;
     private List<Map<String, Object>> recDatabaseList;
 
+    private Map<String, Bitmap> thumbnailBitmaps;
+
     public DownloadRecsAsyncTask(ListView recBookLV, ListView recEBookLV, ListView recDatabaseLV, Context context) {
         this.recBookLVRef = new WeakReference<>(recBookLV);
         this.recEBookLVRef = new WeakReference<>(recEBookLV);
@@ -42,13 +51,33 @@ public class DownloadRecsAsyncTask extends AsyncTask<String, Void, JSONArray> {
         recBookList = new ArrayList<>();
         recEbookList = new ArrayList<>();
         recDatabaseList = new ArrayList<>();
+
+        thumbnailBitmaps = new HashMap<>();
     }
 
     @Override
     protected JSONArray doInBackground(String... shelfNumber) {
         UrlDownloader urlDownloader = new UrlDownloader();
         String url = recApiUrl + shelfNumber[0];
-        return urlDownloader.getArray(url);
+        JSONArray popularItems = urlDownloader.getArray(url);
+
+        JSONObject popularItem;
+        String itemBibId;
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        for (int i = 0; i < popularItems.length(); i++) {
+            try {
+                popularItem = popularItems.getJSONObject(i);
+                if (popularItem != null){
+                    itemBibId = popularItem.getString("bibId");
+                    Bitmap thumbnail = imageLoader.loadImageSync(popularItem.getString("thumbnail"));
+                    thumbnailBitmaps.put(itemBibId, thumbnail);
+                }
+            } catch  (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return popularItems;
     }
 
     @Override
@@ -57,11 +86,10 @@ public class DownloadRecsAsyncTask extends AsyncTask<String, Void, JSONArray> {
         JSONObject popularItem;
         String itemFormat;
         String itemTitle;
-        String itemThumbnailUrl;
         String itemAuthor;
         String itemShelfNumber;
-
-        ImageLoader imageLoader = ImageLoader.getInstance();
+        String itemBibId;
+        Bitmap itemThumbnail;
 
         if (popularItems == null) {
             return;
@@ -74,42 +102,51 @@ public class DownloadRecsAsyncTask extends AsyncTask<String, Void, JSONArray> {
                     itemFormat = popularItem.getString("format");
 
                     if (itemFormat.equals("Book")) {
+                        itemBibId = popularItem.getString("bibId");
                         itemTitle = popularItem.getString("title");
-                        itemThumbnailUrl = popularItem.getString("thumbnail");
                         itemAuthor = popularItem.getString("author");
                         itemShelfNumber = "000";
+                        itemThumbnail = thumbnailBitmaps.get(itemBibId);
 
                         Map<String, Object> map = new HashMap<>();
+                        map.put("recsBibId", itemBibId);
                         map.put("recsTitle", itemTitle);
                         map.put("recsAuthor", "Author: " + itemAuthor);
-                        //map.put("recsThumbnail", imageLoader.loadImageSync(itemThumbnailUrl));
-                        map.put("recsThumbnail", R.drawable.test);
+                        map.put("recsThumbnail", itemThumbnail);
                         map.put("recsShelfNumber", "Shelf Number: " + itemShelfNumber);
 
                         recBookList.add(map);
 
                     } else if (itemFormat.equals("eBook")) {
+                        itemBibId = popularItem.getString("bibId");
                         itemTitle = popularItem.getString("title");
-                        itemThumbnailUrl = popularItem.getString("thumbnail");
                         itemAuthor = popularItem.getString("author");
+                        //itemThumbnail = thumbnailBitmaps.get(itemBibId);
 
                         Map<String, Object> map = new HashMap<>();
+                        map.put("recsBibId", itemBibId);
                         map.put("recsTitle", itemTitle);
                         map.put("recsAuthor", "Author: " + itemAuthor);
-                        //map.put("recsThumbnail", imageLoader.loadImageSync(itemThumbnailUrl));
-                        map.put("recsThumbnail", R.drawable.test);
+                        map.put("recsThumbnail", R.drawable.icon_default_ebook);
 
                         recEbookList.add(map);
 
                     } else if (itemFormat.equals("Database")) {
+                        itemBibId = popularItem.getString("bibId");
                         itemTitle = popularItem.getString("title");
-                        itemThumbnailUrl = popularItem.getString("thumbnail");
                         itemAuthor = popularItem.getString("author");
+                        itemThumbnail = thumbnailBitmaps.get(itemBibId);
 
                         Map<String, Object> map = new HashMap<>();
+                        map.put("recsBibId", itemBibId);
                         map.put("recsTitle", itemTitle);
                         map.put("recsAuthor", "Author: " + itemAuthor);
-                        map.put("recsThumbnail", R.drawable.test);
+
+                        if (itemThumbnail != null) {
+                            map.put("recsThumbnail", itemThumbnail);
+                        } else {
+                            map.put("recsThumbnail", R.drawable.icon_default_database);
+                        }
 
                         recDatabaseList.add(map);
                     }
@@ -122,11 +159,24 @@ public class DownloadRecsAsyncTask extends AsyncTask<String, Void, JSONArray> {
 
         //load contents into the lists
         ListView bookListView = recBookLVRef.get();
-        SimpleAdapter bookAdapter = new SimpleAdapter(displayContext, recBookList, R.layout.recs_list_item_book,
+        ExtendedSimpleAdapter bookAdapter = new ExtendedSimpleAdapter(displayContext, recBookList, R.layout.recs_list_item_book,
                 new String[]{"recsTitle", "recsAuthor", "recsThumbnail", "recsShelfNumber"},
                 new int[]{R.id.recsTitle, R.id.recsAuthor, R.id.recsThumbnail, R.id.recsShelfNumber});
         if (bookAdapter != null) {
             bookListView.setAdapter(bookAdapter);
+            bookListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    // new activity guiding to the new item
+                    String recsBibId = recBookList.get(position).get("recsBibId").toString();
+                    if (recsBibId != null) {
+                        Intent intent = new Intent(displayContext, MainActivity.class);
+                        intent.putExtra("bibId", recsBibId);
+                        displayContext.startActivity(intent);
+                    }
+                }
+            });
         }
 
         ListView ebookListView = recEBookLVRef.get();
@@ -135,6 +185,15 @@ public class DownloadRecsAsyncTask extends AsyncTask<String, Void, JSONArray> {
                 new int[]{R.id.recsTitle, R.id.recsAuthor, R.id.recsThumbnail});
         if (ebookAdapter != null) {
             ebookListView.setAdapter(ebookAdapter);
+            ebookListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    //open up web page within app
+                    Intent intent = new Intent(displayContext, WebActivity.class);
+                    displayContext.startActivity(intent);
+                }
+            });
         }
 
         ListView dbListView = recDatabaseLVRef.get();
@@ -143,8 +202,16 @@ public class DownloadRecsAsyncTask extends AsyncTask<String, Void, JSONArray> {
                 new int[]{R.id.recsTitle, R.id.recsAuthor, R.id.recsThumbnail});
         if (dbAdapter != null) {
             dbListView.setAdapter(dbAdapter);
+            dbListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    //open up web page within app
+                    Intent intent = new Intent(displayContext, WebActivity.class);
+                    displayContext.startActivity(intent);
+                }
+            });
         }
 
     }
-
 }
